334a7c759e02ba7a304d69391a3d97e023199130
mawaheb <mawaheb.sa@gmail.com>
2013-07-15 19:04:21 +0300

    "Initial Commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmSolution::Application.config.secret_token = '8ac9036f2919bf36b1c441bbff1ce7886063b5cf131ad5598f276c90aec539dff748c29851b9885ef5db930794fb5a119595a29ae1e6eae086c461ef4d1cce89'
