ecf41358e5d804133769588498cb3bd30bf54f5b
mawaheb <mawaheb.sa@gmail.com>
2013-07-19 17:17:18 +0300

    nested attributes works, the view is working as well

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,10 @@
 class Client
   include Mongoid::Document
 
-  has_many :contacts, dependent: :delete
+  has_many :contacts
+  accepts_nested_attributes_for :contacts
+
+
 
   STATUS_TYPES = [:not_contacted, :cold_called_not_interrested, :cold_called_req_callback,
                   :cold_called_meeting_get, :gave_demo, :verbal_sign_up_agreement, :sign_up_form_given,
@@ -14,5 +17,8 @@ class Client
   field :company_name,  type: String
   field :sex,           type: Integer, default: SEX.first
   field :status,        type: Symbol,  default: STATUS_TYPES.first
+
+  validates_presence_of :last_name
+  attr_accessible :first_name, :last_name, :company_name, :sex, :status, :contacts_attributes
 
 end
