6c55d280c28966fc01c63dee8747ff79894f0d7f
mawaheb <mawaheb.sa@gmail.com>
2013-07-25 13:28:46 +0300

    - Add meeting dateTime & changed the view according to that.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,6 @@
 class Meeting
   include Mongoid::Document
+  include Mongoid::MultiParameterAttributes
   belongs_to :client
   has_many :contacts
   #has_many :sales_reps
@@ -7,11 +8,12 @@ class Meeting
 
   DEFAULT_DURATION = 2.hours.to_i
 
-  field :memo,                type: String
-  field :du,  as: :duration,  type: Integer, default: DEFAULT_DURATION
+  field :memo,                 type: String
+  field :sd,  as: :start_date, type: DateTime
+  field :du,  as: :duration,   type: Integer, default: DEFAULT_DURATION
 
 #TODO:
-  # field Date / Time.
+  # check if the time should be UTC/Local
   # may be add a meeting objective?
   # Add a method to give a note on Meeting time.
 end
