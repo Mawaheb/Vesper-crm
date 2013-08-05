8da1cb47bb9773f947e35b3ec8a26e59b532cefb
mawaheb <mawaheb.sa@gmail.com>
2013-08-05 15:45:48 +0300

    - Fix meeting Duration, and Modify the view according to that.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -6,11 +6,13 @@ class Meeting
   has_and_belongs_to_many :sales_reps
   #has_many :follow_ups
 
-  DEFAULT_DURATION = 2.hours.to_i
+  DURATION = { :"00:15" => 15, :"00:30"=> 30, :"00:45"=> 45, :"01:00" =>60, :"01:15" =>75,
+                       :"01:30" =>90, :"01:45"=>105, :"02:00" =>120, :"02:15"=>135,
+                       :"02:30" =>150, :"02:45" =>165, :"03:00"=>180 }
 
   field :memo,                 type: String
   field :sd,  as: :start_date, type: DateTime
-  field :du,  as: :duration,   type: Time #, default: DEFAULT_DURATION
+  field :du,  as: :duration,   type: Integer  , default: DURATION[:"00:30"]
 
 #TODO:
   # Fix the Duration field, one suggestion is to store the HH MM in a Separated string fields, and list
