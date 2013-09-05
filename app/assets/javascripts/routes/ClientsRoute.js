9aa903f9a914383f41501682ccb4bc338e41515a
mawaheb <mawaheb.sa@gmail.com>
2013-09-05 21:23:38 +0300

    Fix clients page

--- app/assets/javascripts/routes/ClientsRoute.js
+++ app/assets/javascripts/routes/ClientsRoute.js
@@ -2,10 +2,10 @@ App.ClientsRoute = Ember.Route.extend({
   model: function() {
   	return App.Client.find();
   }
-  setupController: function(controller, context) {
-    this._super(controller, context);
-    this.controllerFor('clientNew').set('model', App.Client.create({
-      name: "new client" }));
-  }
+  // setupController: function(controller, context) {
+  //   this._super(controller, context);
+  //   this.controllerFor('clientNew').set('model', App.Client.create({
+  //     name: "new client" }));
+  // }
 
 });
