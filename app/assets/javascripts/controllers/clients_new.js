7de01639911dc2a9150549ff93736d4d718ba491
mawaheb <mawaheb.sa@gmail.com>
2013-09-05 18:46:36 +0300

    last push

--- app/assets/javascripts/controllers/clients_new.js
+++ app/assets/javascripts/controllers/clients_new.js
@@ -1,8 +1,12 @@
 App.ClientsNewController = Ember.ObjectController.extend({
+	
+	
 	save: function() {
 		this.get("model.transaction").commit();
 		this.get("target").transitionTo("clients");
-	}
+	},
+
+	
 
 
 });
