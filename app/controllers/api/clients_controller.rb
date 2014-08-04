00099ba6080e5cc8038649e2782f898e372eb4df
Mawaheb <mawaheb.sa@gmail.com>
2014-08-04 13:40:54 +0200

    - Add fet_client method & delete Action to ClientsController

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -1,19 +1,30 @@
 class Api::ClientsController < ApplicationController
+  before_filter :fetch_client, :except => [:index, :create]
+  
+  def fetch_client
+    @client = Client.find(params[:id])
+  end
+
   def index
     render json: Client.all
   end
 
   def show
-    render json: Client.find(params[:id])
+    render json: @client
   end
 
-  def update
-    @client = Client.find(params[:id])
-    
+  def update    
     if @client.update_attributes(params[:client])
       head :no_content
     else
       render json: @client.errors, status: :unprocessable_entity
     end 
+  end
+
+  def destroy
+    if @user.destroy
+      head :no_content, status: :ok
+    else
+      render json: @client.errors, status: :unprocessable_entity
   end
 end
